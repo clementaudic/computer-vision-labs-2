c7a30af8852343e65d2bcae333e493f71559833a
clementaudic <59136487+clementaudic@users.noreply.github.com>
2026-01-14 19:33:16 +0100

    add: training results

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11m.pt")
-model.train(data="rocket_dataset.yaml", epochs=20, imgsz=1024, device=-1)
+model.train(data="rocket_dataset.yaml", epochs=20, imgsz=1024, device=[0,1])
